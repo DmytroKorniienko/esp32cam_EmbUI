2668a4ec55de4f4980490a92c47b91246561abea
Dmytro Korniienko <Dmytro.Korniienko@volo.global>
2021-07-08 19:38:37 +0300

    interface update

--- include/interface.h
+++ include/interface.h
@@ -2,7 +2,9 @@
 
 void block_menu(Interface *interf, JsonObject *data);
 void block_cam(Interface *interf, JsonObject *data);
+void block_stream(Interface *interf, JsonObject *data);
 
+void set_refresh(Interface *interf, JsonObject *data);
 void led_toggle(Interface *interf, JsonObject *data);
 void set_led_bright(Interface *interf, JsonObject *data);
 
